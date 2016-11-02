250283eda57e742af8b82cdc8d7190abc75a6900
Eric Panulla <eric.panulla@gmail.com>
2016-11-01 23:43:16 -0400

    Attempt at getting a servlet working..

--- src/main/java/com/ericpanulla/testgradle/MenuApp.java
+++ src/main/java/com/ericpanulla/testgradle/MenuApp.java
@@ -1,17 +1,30 @@
 package com.ericpanulla.testgradle;
 
-import javax.ws.rs.core.Application;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
-public class MenuApp {
+public class MenuApp extends HttpServlet{
+	
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.getWriter().print("Hello from Java built with Gradle!\n");
+    }
 
     public static void main(String[] args) throws Exception{
     	 final Server server = new Server(Integer.valueOf(System.getenv("PORT")));
          final ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
          context.setContextPath("/");
+         server.setHandler(context);
+         context.addServlet(new ServletHolder(new MenuApp()),"/*");
          server.start();
          server.join();  
     }
