b80b8df1a01d76482c661eaa855778e74c68e9fc
Eric Panulla <eric.panulla@gmail.com>
2016-11-01 22:25:42 -0400

    Initial commit.

---
+++ src/main/java/com/ericpanulla/testgradle/HelloWorldService.java
@@ -0,0 +1,21 @@
+package com.ericpanulla.testgradle;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by ewp5080 on 11/1/2016.
+ */
+@Path("/")
+public class HelloWorldService {
+
+    @GET
+    @Path("{param}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMsg(@PathParam("param") String msg) {
+        return "Hello  : " + msg;
+    }
+}
---
+++ src/main/java/com/ericpanulla/testgradle/MenuApplication.java
@@ -0,0 +1,18 @@
+package com.ericpanulla.testgradle;
+
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by ewp5080 on 11/1/2016.
+ */
+public class MenuApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<>();
+        classes.add(HelloWorldService.class);
+        return classes;
+    }
+}
